bca2626fc18e10629bdb4a6ad44db3bb64f28ffd
lmh <lmhatgm@gmail.com>
2014-06-02 15:03:09 -0500

    network security gcd pg90

---
+++ gcd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b, q, r=-1, it = 0;
+	cout << "Enter 2 large integers:" << endl;
+	cin >> a >> b;
+	if(a<b){
+		int temp = b;
+		b = a;
+		a = temp;
+	}
+		
+	while(r!=0){
+		++it;
+		q = a/b;
+		r = a%b;
+		cout << "q" << it << ": " << q << ",\t";
+		cout << "r" << it << ": " << r << ". " << endl;
+		a = b;
+		b = r;	
+	}
+	
+	cout << "gcd(a,b) = " << a << endl;
+	
+	return 0;
+}
